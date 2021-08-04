aec881d4e6393215cd8787e6d450266e614e3b61
mridulhossain <mridulfeni94@gmail.com>
2021-08-04 21:24:55 +0600

    Given an integer x, return true if x is palindrome integer.

---
+++ ProblemSolving/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace ProblemSolving
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(IsPalindrome(0)); 
+
+
+
+
+        }
+
+        public static bool IsPalindrome(int x)
+        {
+            var copy = x;
+            int remainder;
+            long reverse = 0;
+
+            if (x <= 0)
+            {
+                return false;
+            }
+            else
+            {
+                 while (x > 0)
+                    {
+                        remainder = x % 10;
+                        reverse = reverse * 10 + remainder;
+                        x /= 10;
+                    }
+
+                 if(reverse == copy)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+
+                }
+                
+            }
+        }
+
+    }
+}
